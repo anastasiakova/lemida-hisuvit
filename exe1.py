36d39bf8afe1149fbe7d964532a3bd23b66ac666
anastasiakova <kovalkov@post.bgu.ac.il>
2019-11-26 16:31:21 +0200

    shuffle and accur

--- exe1.py
+++ exe1.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import numpy as np
 import sys
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-
 
 def sigmoid_function(matrix_x, theta):
     prediction = np.dot(matrix_x, theta)
@@ -27,7 +24,7 @@ def gradient_descent(matrix_x, vector_of_targets_y, lr=0.1, max_iterations=1000,
     theta = np.zeros((x_with_bias.shape[1], 1))
 
     for i in range(max_iterations):
-        updated_theta = gradient(x_with_bias, vector_of_targets_y, theta)*lr
+        updated_theta = theta - gradient(x_with_bias, vector_of_targets_y, theta)*lr
 
         if root_sum_of_squares(theta, updated_theta) <= threshold:
              break
@@ -44,18 +41,33 @@ def predict(matrix_x, theta):
     prediction = to_binary(sigmoid_prediction)
     return prediction
 
+def splitTrainTest(matrix_x, trainSize):
+    rows = len(matrix_x) * trainSize
+    np.random.shuffle(matrix_x)
+    train_data = matrix_x.iloc[:rows]
+    test_data = matrix_x.iloc[rows:]
+    X_train = train_data.drop(columns=['y'])
+    y_train = train_data['y']
+    X_test = test_data.drop(columns=['y'])
+    y_test = test_data['y']
+    return X_train, y_train, X_test, y_test
+
+def accurancyCalc(prediction, trueValues):
+    right = 0;
+    dataLength = len(prediction)
+    for i in range( dataLength - 1):
+        if prediction[i] == trueValues[i]:
+            right = right + 1
+    return right / dataLength
 
 def main():
-    filePath = sys.argv[1]
+    # filePath = sys.argv[1]
+    filePath ="ex1data1.csv"
     data = pd.read_csv(filePath)
-    matrix_x = data.drop(columns=['y'])
-    y = data['y']
-    X_train, X_test, y_train, y_test = train_test_split(matrix_x, y, test_size=0.2)
+    X_train, y_train, X_test, y_test = splitTrainTest(data, 0.8)
     theta = gradient_descent(np.array(X_train), np.array(y_train))
     prediction = predict(np.array(X_test), theta)
-    accur = accuracy_score(y_test, prediction)
+    accur = accurancyCalc(prediction, y_test)
     print(accur)
-
-
 
 main()
